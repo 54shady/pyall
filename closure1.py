c62e20a6907b3eb84fa453c34e86c98599b3096b
zeroway <M_O_Bz@163.com>
2018-06-07 23:40:18 +0000

    closure example update

---
+++ closure1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# import local debug info module
+from print_debug_info import pdi
+
+def test(number):
+    pdi()
+
+    '''在函数内部再定义一个函数,并且这个内部函数用到了外边函数的形参'''
+    '''那么将这个函数及变量称作闭包'''
+    '''closure就是一个闭包'''
+    def closure():
+        pdi()
+        print(number + 99)
+
+    pdi()
+    return closure()
+
+test(100)
