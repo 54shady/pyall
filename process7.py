68193bf44c191663accdff012d50f82fb663c6e6
zeroway <M_O_Bz@163.com>
2018-06-28 23:29:41 +0000

    Queue usage

---
+++ process7.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# ps aux 查看只要子进程不结束的话主进程也不会结束
+# 这点和C里的不同
+# 在C语言里主进程不主动等待子进程的话主进程就会结束
+from multiprocessing import Process
+import time
+
+NUMBER=5
+
+def start_routine(name, index):
+    while True:
+        print 'name = %s, index = %d' % (name , index)
+        time.sleep(1)
+
+# 传给子进程的参数写在args的元组里
+# 最后一个要留空
+p = Process(target=start_routine, args=("debug", 999, ))
+p.start()
+
+# 主进程阻塞在这里等待子进程(NUMBER秒)结束后才继续往下走
+# p.join() 不传参数表示永远等待
+p.join(NUMBER)
+print('main...')
---
+++ queue2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from multiprocessing import Process, Queue
+import os, time, random
+
+# 使用队列来实现进程间通信
+
+# 不断向队列中写消息
+def write(q):
+    while True:
+        if not q.full():
+            for value in str(random.random()):
+                print '==> %s' % value
+                q.put(value)
+                time.sleep(random.random())
+        else:
+            time.sleep(2)
+
+# 不断读取队列中消息
+def read(q):
+    while True:
+        if not q.empty():
+            v = q.get(True) # 等价于q.get_nowait()非阻塞
+            print '<== %s' % v
+            time.sleep(random.random())
+        else:
+            time.sleep(1)
+
+if __name__ == '__main__':
+    # 父进程创建消息队列,并传给子进程
+    q = Queue(10)
+    pw = Process(target=write, args=(q,))
+    pr = Process(target=read, args=(q,))
+
+    # 启动子进程写消息
+    pw.start()
+
+    # 启动读进程读消息
+    pr.start()
+
+    # 等待读进程读完所有消息
+    pr.join()
+
+    # 等待写进程结束
+    pw.join()
