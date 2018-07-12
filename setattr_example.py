163c0e43191066526c64f16c2b9d130009f73298
zeroway <M_O_Bz@163.com>
2018-07-12 10:13:44 +0800

    Using has and get attr function

--- setattr_example.py
+++ setattr_example.py
@@ -6,15 +6,29 @@ class DemoCls(object):
         for k, v in kwargs.iteritems():
             setattr(self, k, v) # self.k = v
 
+    def get_cls_attr(self, key_name):
+        print getattr(self, key_name)
+
+    def has_attr(self, key_name):
+        return hasattr(self, key_name)
+
 def main():
     dcls = DemoCls(
             key1="value1",
             key2="value2",
             key3="value3")
 
-    print dcls.key1
-    print dcls.key2
-    print dcls.key3
+    if dcls.has_attr("key1") :
+        dcls.get_cls_attr("key1")
+
+    if dcls.has_attr("key2"):
+        dcls.get_cls_attr("key2")
+
+    if dcls.has_attr("key3"):
+        dcls.get_cls_attr("key3")
+
+    if dcls.has_attr("key4"):
+        dcls.get_cls_attr("key4")
 
 if __name__ == '__main__':
     main()
