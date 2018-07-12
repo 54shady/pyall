7605ed7972553f650e97028c247c6891ca8e0015
zeroway <M_O_Bz@163.com>
2018-07-12 20:41:34 +0800

    Init class method using load module

---
+++ module_imp/example2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import imp
+import sys
+
+class TestCls():
+    module = None
+
+    def __init__(self, **kwargs):
+        # 批量初始化类属性
+        for k, v in kwargs.iteritems():
+          setattr(self, k, v)
+
+        if self.module is None:
+            # 通过加载模块来初始化类方法
+            info = imp.find_module('say_hello', ['./localmodule/'])
+            self.module = imp.load_module("hello", *info)
+            print(dir(self.module))
+
+    def _DoCall(self, function_name, *args, **kwargs):
+        if self.module is None or not hasattr(self.module, function_name):
+            return kwargs.get("default", None)
+
+        return getattr(self.module, function_name)()
+
+    def SayHello(self):
+        return self._DoCall("sayHello")
+
+def main():
+    tObj = TestCls(key1 = "value1",
+                key2 = "value2",
+                key3 = "value3")
+
+    print tObj.key1
+    print tObj.key2
+    print tObj.key3
+
+    #print dir(tObj)
+    tObj.SayHello()
+
+if __name__ == '__main__':
+    main()
--- module_imp/localmodule/say_goodbye.py
+++ module_imp/localmodule/say_goodbye.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-def main():
+def say_goodbye():
     print 'say goodbye.....'
