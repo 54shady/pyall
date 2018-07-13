a2418fc620403f9ea8b8c148c5769098a6309ee9
zeroway <M_O_Bz@163.com>
2018-07-13 10:15:31 +0800

    Set : unordered collection of distinct hashtable objects

--- edit_tags.py
+++ edit_tags.py
@@ -7,19 +7,24 @@ tags_changes = ('-/xxoo/tags', '+/this/tags')
 def EditTags(tags):
     """Given a string containing comma-separated tags, apply the edits
   specified in tag_changes and return the updated string."""
+
+    # set的元素没有重复,而且是无序的
+    # 使用set()将重复的元素删除
     tags = set(tags.split(","))
 
     for ch in tags_changes:
         if ch[0] == "-":
             tags.discard(ch[1:])
-            print 'remove %s' % ch[1:]
+            #print 'remove %s' % ch[1:]
         elif ch[0] == "+":
             tags.add(ch[1:])
-            print 'add %s' % ch[1:]
-        return ",".join(sorted(tags))
+            #print 'add %s' % ch[1:]
+
+    return ",".join(sorted(tags))
 
 def main():
     oldtags = (
+            "/home/tags,"
             "/home/tags,"
             "/xxoo/tags,"
             "/opt/tags,"
