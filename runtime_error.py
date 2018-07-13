7e1374d3c0e4211e609059d52a85ce1df66829c5
zeroway <M_O_Bz@163.com>
2018-07-13 14:40:58 +0800

    How to raise an runtime error

---
+++ runtime_error.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+class ExternalError(RuntimeError):
+    pass
+
+def main():
+    v = input()
+    if v != 1:
+        raise ExternalError('input value %d error(must be 1)' % v)
+
+if __name__ == '__main__':
+    main()
