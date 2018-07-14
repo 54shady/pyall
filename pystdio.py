0350269a70ca6d71dcf1e43677537e9ca3850bab
zeroway <M_O_Bz@163.com>
2018-07-14 15:24:30 +0800

    Read data from stdin

---
+++ pystdio.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import sys
+
+""" usage
+cat a_file | python pystdio.py
+:!cat a_file | python %
+"""
+
+def main():
+    # read from stdin
+    lines = sys.stdin.readlines()
+
+    # print out the data
+    for i in range(len(lines)):
+        line = lines[i].rstrip()
+        print line
+
+if __name__ == '__main__':
+    main()
