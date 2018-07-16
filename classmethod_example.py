c67fb5adf063a3a60b3416d5838fb540adee4b17
zeroway <M_O_Bz@163.com>
2018-07-16 19:47:05 +0800

    Update file format meet with the PEP8

--- classmethod_example.py
+++ classmethod_example.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-# 调用类方法实现打开一个文件
-# 并将文件内容保存,且记录文件相应名字
-
 
 class MyFileClass(object):
+    """
+    调用类方法实现打开一个文件
+    并将文件内容保存,且记录文件相应名字
+    """
+
     def __init__(self, name, data):
         self.name = name
         self.data = data
-    '''
-    nickname : is an alias name for the filename
-    filename : is the real name on the disk
-    example :
-        filename maybe "/path/to/the/realfile.txt"
-        nickname maybe "realfile"
-    '''
+
     @classmethod
     def load_file(cls, nickname, filename):
+        '''
+        nickname : is an alias name for the filename
+        filename : is the real name on the disk
+        example :
+            filename maybe "/path/to/the/realfile.txt"
+            nickname maybe "realfile"
+        '''
         with open(filename) as f:
             data = f.read()
             return MyFileClass(nickname, data)
